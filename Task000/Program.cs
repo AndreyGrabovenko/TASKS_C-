8deab13f45df0c9ca3a27a427ced58c425808ce7
Андрей <grabovenko.andrei@mail.ru>
2025-02-27 19:30:35 +0300

    Task000

---
+++ Task000/Program.cs
@@ -0,0 +1,12 @@
+﻿//  0. Вывести квадрат числа
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int square = number * number;
+        Console.WriteLine("Квадрат числа {0} равен {1}", number, square);
+    }
+}
